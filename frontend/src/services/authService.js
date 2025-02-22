09e851d62b5e715726cf6131f396caeca34af51e
hammdi <hamdi.kbaier@esprit.tn>
2025-02-22 12:37:16 +0100

    CRUD+mail+fix 0.4.4

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/auth'; // Change selon ton backend
+
+export const getCurrentUser = async () => {
+    try {
+        const token = localStorage.getItem('token'); // Récupérer le token depuis le stockage local
+        console.log(token);
+        if (!token) return null;
+
+        const response = await axios.get(`${API_URL}/me`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error('Erreur lors de la récupération de l’utilisateur', error);
+        return null;
+    }
+};
