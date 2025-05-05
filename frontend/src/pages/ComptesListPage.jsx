ca4aba615ff43d2b6cbcd87d372742852248e69f
Said Ahmed Boudali <saidahmedboudali.s@gmail.com>
2025-05-05 15:37:43 +0100

    accounting entity fixing the accounting plan with financial statements frontback v1.0

--- frontend/src/pages/ComptesListPage.jsx
+++ frontend/src/pages/ComptesListPage.jsx
@@ -1,49 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import MasterLayout from "../masterLayout/MasterLayout";
 
-const ComptesListPage = () => {
-    const [comptes, setComptes] = useState([]);
+const AccountsListPage = () => {
+    const [accounts, setAccounts] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:5000/api/comptes')
-            .then(res => setComptes(res.data))
-            .catch(err => console.error("❌ Error fetching comptes:", err));
+            .then(res => setAccounts(res.data))
+            .catch(err => console.error("❌ Error fetching accounts:", err));
     }, []);
 
     return (
-        <div className="col-lg-10 mx-auto">
-            <div className="card">
-                <div className="card-header">
-                    <h5 className="card-title mb-0">📘 Plan Comptable</h5>
-                </div>
-                <div className="card-body">
-                    <div className="table-responsive">
-                        <table className="table basic-table mb-0">
-                            <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Numéro</th>
-                                <th>Libellé</th>
-                                <th>Niveau</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {comptes.map((compte, index) => (
-                                <tr key={compte._id}>
-                                    <td>{index + 1}</td>
-                                    <td>{compte.Num}</td>
-                                    <td>{compte.Libelle}</td>
-                                    <td>{compte.Level}</td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
+        <>
+            <MasterLayout>
+
+                <div className="col-lg-10 mx-auto">
+                    <div className="card">
+                        <div className="card-header">
+                            <h5 className="card-title mb-0">📘 Accounting plan</h5>
+                        </div>
+                        <div className="card-body">
+                            <div className="table-responsive">
+                                <table className="table basic-table mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Account Number</th>
+                                            <th>Label</th>
+                                            <th>Level</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {accounts.map((account, index) => (
+                                            <tr key={account._id}>
+                                                <td>{index + 1}</td>
+                                                <td>{account.Num}</td>
+                                                <td>{account.Libelle}</td>
+                                                <td>{account.Level}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
 
+            </MasterLayout>
+        </>
     );
 };
 
-export default ComptesListPage;
+export default AccountsListPage;
